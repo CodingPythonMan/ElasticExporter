64f9af4597e416e470da0586e23c904f1ab8c3d3
김주영 <robolab1902@ringgames.com>
2022-10-14 14:40:30 +0900

    Config 추가

--- StatisticsExporter/Program.cs
+++ StatisticsExporter/Program.cs
@@ -5,8 +5,3 @@ ConfigService configService = new();
 DailyService dailyService = new();
 
 dailyService.Start(ConfigService._Config.env.dailyServiceValid).Wait();
-
-foreach(var item in args)
-{
-    Console.WriteLine(args);
-}
