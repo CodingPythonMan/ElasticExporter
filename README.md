0008fcf32ae795d7042cf3a378992191bd8953db
JuYoung Kim <robolab1902@naver.com>
2023-12-06 15:43:13 +0900

    다른 폴더와 프로젝트 합침. 이제 이 프로젝트는 RedBlack 으로

--- README.md
+++
@@ -1,5 +0,0 @@
-# Statistics Exporter
-- ElasticSearch Index 들 정리하여 CSV 로 출력 자동화
-## 문제상황 정리
-- Log 데이터들을 정보로 가공하기
-- C# 에서 제공하는 ElasticSearch 관련 툴인 Nest 활용
--- StatisticsExporter/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using StatisticsExporter.Service;
-using StatisticsExporter.Service.Automation;
-
-ConfigService configService = new();
-DailyService dailyService = new();
-
-dailyService.Start(ConfigService._Config.env.dailyServiceValid).Wait();
